90999548ce896d9157f3a8cf86312c9a8b6b5187
RobinSanta <robinsntn@gmail.com>
2019-12-04 16:45:20 -0800

    the hunter create function is complete

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -6,4 +6,19 @@ class HuntersController < ApplicationController
     def show 
         @hunter = Hunter.find(params[:id])
     end
+
+    def new
+        @hunter = Hunter.new
+    end
+
+    def create
+        @hunter = Hunter.new(hunter_params)
+        @hunter.save
+        redirect_to @hunter
+    end
+
+    private
+    def hunter_params
+        params.require(:hunter).permit(:name, :hp, :speed)
+    end
 end
