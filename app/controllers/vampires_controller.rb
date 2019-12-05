a5d89f8f515919d6a756c4bbd684d7c022517a1b
RobinSanta <robinsntn@gmail.com>
2019-12-04 17:00:29 -0800

    added CRUD methods t vampire and got index working

--- app/controllers/vampires_controller.rb
+++ app/controllers/vampires_controller.rb
@@ -1,2 +1,38 @@
 class VampiresController < ApplicationController
+    def index
+        @vampires = Vampire.all
+    end
+
+    def show 
+        locate
+    end
+
+    def new
+        @vampire = Vampire.new
+    end
+
+    def create
+        @vampire = Vampire.new(vampire_params)
+        @vampire.save
+        redirect_to @vampire
+    end
+
+    def edit
+        locate
+    end
+
+    def update
+        locate
+        @vampire.update(vampire_params)
+        redirect_to @vampire
+    end
+
+    private
+    def vampire_params
+        params.require(:vampire).permit(:name, :hp, :speed)
+    end
+
+    def locate
+        @vampire = Vampire.find(params[:id])
+    end
 end
