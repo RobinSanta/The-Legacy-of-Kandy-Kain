44e3c145e9a124dd7bdc407a60b86c605357a40b
RobinSanta <robinsntn@gmail.com>
2019-12-05 17:28:34 -0800

    Fighting works now. Just gotta add a button so it's not automatic

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -13,7 +13,21 @@ class Encounter < ApplicationRecord
         end
     end
 
-    # def fight
-    #     if 
-    # end
+    def fight
+        vampire = Vampire.find(self.vampire_id)
+        hunter = Hunter.find(self.hunter_id)
+        while vampire.hp > 0 || hunter.hp > 0
+            if vampire.speed >= hunter.speed
+                hunter.hp -= vampire.abilities.first.damage
+            else
+                vampire.hp -= hunter.weapons.first.damage
+            end
+            if vampire.hp <= 0
+                return "#{vampire.name} has been slain! Vae Victis!"
+            elsif hunter.hp <= 0
+                return "#{hunter.name}'s blood will feed #{vampire.name}. Vae Victis!"
+            end
+        end
+    end
+
 end
